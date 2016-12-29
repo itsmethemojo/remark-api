bfb593585d0fd32b53b26ed31df648d96ff72736
itsmethemojo <marianpoeschmann@googlemail.com>
2016-12-29 19:13:44 +0100

    tune marking

--- public/javascript/remark.js
+++ public/javascript/remark.js
@@ -218,11 +218,14 @@ Remark.prototype.getRemarkVisibility = function (count) {
         case 0:
             return 0;
         case 1:
-            return 2;
+            return 0;
         case 2:
-            return 4;
+            return 2;
         case 3:
+            return 4;
+        case 4:
             return 6;
+
     }
     return 8;
 }
